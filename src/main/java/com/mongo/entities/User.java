e42a48008fc361341b5378234464c3fade18cf05
AnkurDesai11 <60006478+AnkurDesai11@users.noreply.github.com>
2022-04-19 01:46:06 +0530

    Update User.java

--- src/main/java/com/mongo/entities/User.java
+++ src/main/java/com/mongo/entities/User.java
@@ -1,10 +1,12 @@
 package com.mongo.entities;
 
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "biodata")
 public class User {
 	
+	@Id
 	private int id;
 	private String name;
 	private int age;
